093802829479797356c851315d03af99df5b9708
Snobeard <fatmattmatt@gmail.com>
2017-10-26 10:43:54 -0700

    set up option select menu

--- mattL-dre/scripts/article.js
+++ mattL-dre/scripts/article.js
@@ -19,7 +19,6 @@ Article.prototype.toHtml = function() {
   }
   $newArticle.attr('data-category', this.category);
   // DONE: Use jQuery to add the author name as an additional data-attribute of the newly cloned article. Doing so will allow us to use selectors to target articles based on who wrote them.
-
   $newArticle.attr('data-author', this.author);
 
   $newArticle.find('.byline a').html(this.author);
--- mattL-dre/scripts/articleView.js
+++ mattL-dre/scripts/articleView.js
@@ -38,16 +38,16 @@ articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
     // REVIEW: Inside this function, "this" is the element that triggered the event handler function we are defining. "$(this)" is using jQuery to select that element (analogous to event.target that we have seen before), so we can chain jQuery methods onto it.
     if ($(this).val()) {
-      // TODO: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+      // DONE?: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
       // Use an "attribute selector" to find those articles, and fade them in for the reader.
       $('#articles div').siblings().hide();
-      $(this).data('category');
+      $('#articles').find('article').attr('data-author', $(this).val()).show();
+      console.log($('#articles').find('article').attr('data-author'));
 
     } else {
       // DONE: If the <select> menu was changed to an option that is blank, we should first show all the articles, except the one article we are using as a template.
       $('#articles div').siblings().hide();
       $('#articles div').siblings().not('.template').fadeIn();
-
     }
     $('#category-filter').val('');
   });
@@ -80,5 +80,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
